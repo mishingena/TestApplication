41677831d13e1b1dcc794fea400f6e0e11ce17d2
Gena <mishingena@gmail.com>
2018-12-01 01:02:09 +0300

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Gennadiy Mishin on 01/12/2018.
+//  Copyright © 2018 Gennadiy Mishin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
